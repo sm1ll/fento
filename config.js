9f7ea4e62c9e0af229bd8bfbc8d6f434903b6e38
sm1ll <140668708+sm1ll@users.noreply.github.com>
2026-05-17 19:44:59 -0400

    Change weather API key in config.js
    Updated the weather API key in the configuration.

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '9113a84ae0deb23def7710e61ad2467c', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
